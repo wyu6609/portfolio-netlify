0627938d6b14ac4fe11883772816c5968d8d2b93
William Yu <88666309+wyu6609@users.noreply.github.com>
2022-06-03 00:17:23 -0400

    hover

--- src/portfolio-content/Projects.js
+++ src/portfolio-content/Projects.js
@@ -118,7 +118,7 @@ const Projects = () => {
                   display: "flex",
                   flexDirection: "column",
                   "&:hover": {
-                    backgroundColor: "#eeeeee",
+                    backgroundColor: "white",
                   },
                 }}
               >
