74f5a8e693ecc350d96a8ccc5723a0ff37b5055d
Will <wy427@nyu.edu>
2023-03-28 19:40:19 -0400

    mobile responsive iframe

--- src/portfolio-content/Hobbies.js
+++ src/portfolio-content/Hobbies.js
@@ -1,10 +1,11 @@
 import React, { useState } from "react";
+import "./Hobbies.css";
 import { Document, Page } from "react-pdf";
 import resume from "./Resume.pdf";
 import resumePDF from "./Resume.pdf";
 import { Grid, Paper } from "@mui/material";
 import {
-    
+    Typography,
     Container,
   
 
@@ -15,16 +16,21 @@ const Hobbies = () => {
 
   return (
     <Container
+   
     sx={{
       pb: 3,
       alignItems: "center",
       backgroundColor: "#eaeff1",
       border: "none",
     }}
-  >     <h3>March 16 at 2:40 PM</h3>
-        <p>I play Nocturne in C Sharp Minor by Chopin</p>
-        
+  >     <Typography color="textPrimary" gutterBottom variant="h1" sx= {{fontSize:12, fontWeight:'bold'}}>March 16 at 2:40 PM</Typography>
+       
+           <Typography color="textSecondary" gutterBottom variant="p4" sx= {{fontSize:12}}>
+           I play Nocturne in C Sharp Minor by Chopin
+        </Typography>
+        <div className = "iframe-container" >
         <iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/DBpKZhytozs" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+        </div>
     </Container>
   );
 };
