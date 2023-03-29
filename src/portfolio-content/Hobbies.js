01736f1dcdc1fb8fbb6521fff3f1986e897bfcc4
Will <wy427@nyu.edu>
2023-03-28 20:14:27 -0400

    blog update

--- src/portfolio-content/Hobbies.js
+++ src/portfolio-content/Hobbies.js
@@ -7,8 +7,11 @@ import { Grid, Paper } from "@mui/material";
 import {
     Typography,
     Container,
-  
-
+    Card,
+    CardActions,
+    CardContent,
+    Box
+    
   } from "@mui/material"
 
 import "./Resume.css";
@@ -23,14 +26,65 @@ const Hobbies = () => {
       backgroundColor: "#eaeff1",
       border: "none",
     }}
-  >     <Typography color="textPrimary" gutterBottom variant="h1" sx= {{fontSize:12, fontWeight:'bold'}}>March 16 at 2:40 PM</Typography>
-       
-           <Typography color="textPrimary" gutterBottom variant="p4" sx= {{fontSize:12}}>
-           I play Nocturne in C Sharp Minor by Chopin
+  >   
+  
+  <Card sx={{ minWidth: 275 }}>
+      <CardContent>
+        <Typography sx={{ fontSize: 14, fontWeight: 'bold' }} color="text.secondary" gutterBottom>
+        March 6 2023 at 4:40 PM
+        </Typography>
+        
+        <Typography sx={{ mb: 1.5 }} color="text.secondary">
+        I play Nocturne in C Sharp Minor by Chopin
         </Typography>
         <div className = "iframe-container" >
         <iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/DBpKZhytozs" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
         </div>
+      </CardContent>
+      <CardActions>
+
+      </CardActions>
+    </Card>  
+
+    <Card sx={{ minWidth: 275, mt: 2 }}>
+      <CardContent>
+        <Typography sx={{ fontSize: 14, fontWeight: 'bold' }} color="text.secondary" gutterBottom>
+        September 09 2022 at 2:20 PM
+        </Typography>
+        
+        <Typography sx={{ mb: 1.5 }} color="text.secondary">
+        Lorem ipsum is placeholder text.
+        </Typography>
+        <div  >
+        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </div>
+      </CardContent>
+      <CardActions>
+
+      </CardActions>
+    </Card>  
+
+   
+
+
+
+    <Card sx={{ minWidth: 275, mt: 2 }}>
+      <CardContent>
+        <Typography sx={{ fontSize: 14, fontWeight: 'bold' }} color="text.secondary" gutterBottom>
+        August 20 2022 at 5:20 PM
+        </Typography>
+        
+        <Typography sx={{ mb: 1.5 }} color="text.secondary">
+        Lorem ipsum is placeholder text.
+      
+        </Typography>
+        <div class="tenor-gif-embed" data-postid="17252607" data-share-method="host" data-aspect-ratio="1.15942" data-width="100%"><a href="https://tenor.com/view/do-not-touch-it-programmer-walking-cow-coding-gif-17252607">Do Not Touch It Programmer GIF</a>from <a href="https://tenor.com/search/do+not+touch+it-gifs">Do Not Touch It GIFs</a></div> 
+      </CardContent>
+      <CardActions>
+
+      </CardActions>
+    </Card>  
+     
     </Container>
   );
 };
