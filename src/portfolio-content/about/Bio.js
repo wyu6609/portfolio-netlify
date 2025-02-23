898e6be4fedfbe7b520b8a2654ab6af89d45c844
Will <wy427@nyu.edu>
2025-02-23 10:42:47 -0500

    layout fix

--- src/portfolio-content/about/Bio.js
+++ src/portfolio-content/about/Bio.js
@@ -7,30 +7,34 @@ import "./Bio.css";
 const Bio = ({ setSelectedIndex, btnSound }) => {
   let navigate = useNavigate();
   return (
-    <>
-      <Box
-        component="main"
-        sx={{
-          flexGrow: 1,
-          py: { xs: 1, sm: 8 },
-        }}
-      >
-        <Container maxWidth="lg">
-          <Grid container spacing={3}>
-            <Grid item lg={6} md={6} xs={12}>
-              <AccountProfile />
-            </Grid>
-            <Grid item lg={6} md={6} xs={12}>
-              <AccountProfileDetails
-                setSelectedIndex={setSelectedIndex}
-                btnSound={btnSound}
-                navigate={navigate}
-              />
-            </Grid>
+    <Box
+      component="main"
+      sx={{
+        flexGrow: 1,
+        py: { xs: 1, sm: 8 },
+        maxWidth: "100%",
+        overflow: "hidden",
+      }}
+    >
+      <Container maxWidth="lg" sx={{ maxWidth: "100%", overflow: "hidden" }}>
+        <Grid
+          container
+          spacing={3}
+          sx={{ maxWidth: "100%", overflow: "hidden" }}
+        >
+          <Grid item lg={6} md={6} xs={12}>
+            <AccountProfile />
           </Grid>
-        </Container>
-      </Box>
-    </>
+          <Grid item lg={6} md={6} xs={12}>
+            <AccountProfileDetails
+              setSelectedIndex={setSelectedIndex}
+              btnSound={btnSound}
+              navigate={navigate}
+            />
+          </Grid>
+        </Grid>
+      </Container>
+    </Box>
   );
 };
 
