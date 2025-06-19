e6caba4030f0dd3a0450385d127c2fc2bde29278
Will <wy427@nyu.edu>
2025-06-19 10:40:16 -0400

    add linkedin badge

--- src/portfolio-content/about/account-profile.js
+++ src/portfolio-content/about/account-profile.js
@@ -14,6 +14,7 @@ import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import GitHubIcon from "@mui/icons-material/GitHub";
 import ArticleIcon from "@mui/icons-material/Article";
 import { AccountProfileDetails } from "./account-profile-details";
+import Modal from "@mui/material/Modal";
 
 const user = {
   avatar: "../../images/avatar.png",
@@ -27,6 +28,7 @@ const user = {
 export const AccountProfile = (props) => {
   const detailsRef = useRef(null);
   const [isVisible, setIsVisible] = useState(false);
+  const [open, setOpen] = useState(false);
 
   useEffect(() => {
     const observer = new IntersectionObserver(
@@ -53,6 +55,20 @@ export const AccountProfile = (props) => {
     };
   }, []);
 
+  // Load LinkedIn badge script when modal opens
+  useEffect(() => {
+    if (open) {
+      const script = document.createElement("script");
+      script.src = "https://platform.linkedin.com/badges/js/profile.js";
+      script.async = true;
+      script.defer = true;
+      document.body.appendChild(script);
+      return () => {
+        document.body.removeChild(script);
+      };
+    }
+  }, [open]);
+
   return (
     <Card {...props} sx={{ backgroundColor: "#ffffff" }}>
       <CardContent>
@@ -77,6 +93,67 @@ export const AccountProfile = (props) => {
             <Typography color="textPrimary" gutterBottom variant="h4">
               {user.name}
             </Typography>
+            {/* LinkedIn Badge Button */}
+            <Button
+              variant="contained"
+              color="primary"
+              sx={{
+                mt: 1,
+                mb: 2,
+                alignSelf: "center",
+                px: 4,
+                py: 1.2,
+                fontWeight: 600,
+                fontSize: "1rem",
+                borderRadius: 3,
+                boxShadow: 2,
+                letterSpacing: 1,
+                transition: "background 0.2s, transform 0.2s",
+                "&:hover": {
+                  backgroundColor: "#1976d2",
+                  transform: "scale(1.05)",
+                },
+              }}
+              onClick={() => setOpen(true)}
+            >
+              View LinkedIn Badge
+            </Button>
+            {/* Modal for LinkedIn Badge */}
+            <Modal
+              open={open}
+              onClose={() => setOpen(false)}
+              aria-labelledby="linkedin-badge-modal"
+              aria-describedby="linkedin-badge-modal-description"
+              sx={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+            >
+              <Box
+                sx={{
+                  bgcolor: "background.paper",
+                  boxShadow: 24,
+                  p: 4,
+                  borderRadius: 2,
+                  outline: "none",
+                  display: "flex",
+                  flexDirection: "column",
+                  alignItems: "center",
+                }}
+              >
+                <div
+                  className="badge-base LI-profile-badge"
+                  data-locale="en_US"
+                  data-size="large"
+                  data-theme="light"
+                  data-type="HORIZONTAL"
+                  data-vanity="will-yu-56b101a8"
+                  data-version="v1"
+                ></div>
+              </Box>
+            </Modal>
+            {/* End LinkedIn Badge Modal */}
             <Typography color="textSecondary" align="center" variant="body2">
               {`${user.skills} `}
             </Typography>
@@ -88,34 +165,60 @@ export const AccountProfile = (props) => {
             >
               {user.skills2}
             </Typography>
+            {/* Social Buttons Row */}
+            <Box
+              sx={{
+                display: "flex",
+                justifyContent: "center",
+                gap: 2,
+                mt: 2,
+                mb: 1,
+              }}
+            >
+              <Button
+                component={Link}
+                href="https://www.linkedin.com/in/will-yu-56b101a8?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3B3IFmW0pcQTeRea%2FmutKVsw%3D%3D"
+                target="_blank"
+                sx={{
+                  minWidth: 48,
+                  color: "#0a66c2",
+                  "&:hover": { bgcolor: "#eaf4fc" },
+                }}
+              >
+                <LinkedInIcon fontSize="large" />
+              </Button>
+              <Button
+                component={Link}
+                href="https://github.com/wyu6609"
+                target="_blank"
+                sx={{
+                  minWidth: 48,
+                  color: "#24292f",
+                  "&:hover": { bgcolor: "#f6f8fa" },
+                }}
+              >
+                <GitHubIcon fontSize="large" />
+              </Button>
+              <Button
+                component={Link}
+                href="https://docs.google.com/document/d/1yT5g5qLfffm5N2rtVY4ryuQgEpu9_L9wLoU5pW948E8/edit?usp=sharing"
+                target="_blank"
+                sx={{
+                  minWidth: 48,
+                  color: "#d2691e",
+                  "&:hover": { bgcolor: "#fff3e0" },
+                }}
+              >
+                <ArticleIcon fontSize="large" />
+              </Button>
+            </Box>
           </Box>
           <Typography
             sx={{ mt: 2 }}
             variant="body2"
             color="primary.main"
             align="center"
-          >
-            <Link
-              href="https://www.linkedin.com/in/will-yu-56b101a8?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3B3IFmW0pcQTeRea%2FmutKVsw%3D%3D"
-              target="_blank"
-            >
-              <LinkedInIcon className="fancy_card1" fontSize="large" />
-            </Link>
-            <Link
-              href="https://github.com/wyu6609"
-              target="_blank"
-              sx={{ p: 0.5 }}
-            >
-              <GitHubIcon className="fancy_card1" fontSize="large" />
-            </Link>
-            <Link
-              href="https://docs.google.com/document/d/1yT5g5qLfffm5N2rtVY4ryuQgEpu9_L9wLoU5pW948E8/edit?usp=sharing"
-              target="_blank"
-              sx={{ p: 0.5 }}
-            >
-              <ArticleIcon className="fancy_card1" fontSize="large" />
-            </Link>
-          </Typography>
+          ></Typography>
         </Card>
         <Divider sx={{ m: 2 }} />
         <div
